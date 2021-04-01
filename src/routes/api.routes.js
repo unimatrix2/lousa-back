0f358b62301ed3f35fb1cc1e38d149b37727b414
unimatrix2 <dnny.nis@gmail.com>
2021-04-01 18:03:40 -0300

    added routing file

---
+++ src/routes/api.routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import routeProtection from '../middlewares/routeProtection';
+import authRoutes from './auth/auth.routes';
+import userRoutes from './user/user.routes';
+import postRoutes from './post/post.routes';
+
+const router = Router();
+
+router.use('/auth', authRoutes);
+router.use('/user', userRoutes);
+router.use(routeProtection);
+router.use('/post', postRoutes);
+
+export default router;
