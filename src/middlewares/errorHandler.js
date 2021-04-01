6be07a4643d8193367a2079db1250fd7ca108ae4
unimatrix2 <dnny.nis@gmail.com>
2021-04-01 17:56:14 -0300

    linted server error handling

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,17 +1,16 @@
 /* eslint-disable no-console */
 export const handleError = (err, req, res) => {
-  console.log(err);
-  const { message, type, status } = err;
-  const statusCode = status || 500;
-
-  return res.status(statusCode).json({ message, type: type || '', status: statusCode });
+	console.log(err);
+	const { message, type, status } = err;
+	const statusCode = status || 500;
+	return res.status(statusCode).json({ message, type: type || '', status: statusCode });
 };
 
 export const handle404 = (req, res) => {
-  res.status(404).json({
-    name: 'Page404',
-    url: req.url,
-    message: 'Service not found',
-    status: 404,
-  });
+	res.status(404).json({
+		name: 'Page404',
+		url: req.url,
+		message: 'Service not found',
+		status: 404,
+	});
 };
