14123074c860dd05148f9ff19ed70a30bdf52f8d
unimatrix2 <dnny.nis@gmail.com>
2021-04-02 18:02:10 -0300

    [AUTH] Modularized responses

--- src/routes/auth/auth.routes.js
+++ src/routes/auth/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { validateSignup, validateLogin } from '../../middlewares/authValidation';
-import { register } from '../../services/User.service';
+import { login, register } from '../../services/User.service';
+import { authResponse, authBadResponse } from '../../utils/authManager';
 
 const router = Router();
 
@@ -8,19 +9,19 @@ router.post('/signup', validateSignup, async (req, res) => {
 	try {
 		const { body } = req;
 		const payload = await register(body);
-		res.cookie('token', payload.token, {
-			maxAge: process.env.COOKIE_EXPIRY,
-			httpOnly: true,
-			signed: true,
-			sameSite: 'strict',
-			secure: true,
-		});
-		res.json(payload.user).status(200);
-	} catch (error) {
-		res.json({
-			message: 'An error has occured, please try again later',
-			error,
-		}).status(error.status);
+		authResponse(res, payload);
+	} catch (err) {
+		authBadResponse(res, err);
+	}
+});
+
+router.post('/login', validateLogin, async (req, res) => {
+	try {
+		const { body } = req;
+		const payload = await login(body);
+		authResponse(res, payload);
+	} catch (err) {
+		authBadResponse(res, err);
 	}
 });
 
