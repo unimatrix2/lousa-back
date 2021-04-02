aa719164433a402bea7211b56ee835202ee73581
unimatrix2 <dnny.nis@gmail.com>
2021-04-02 18:07:26 -0300

    [AUTH] Implemented Login

--- src/services/User.service.js
+++ src/services/User.service.js
@@ -1,12 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import AppError from '../errors/AppError';
-import { createUser, checkExistingUser } from '../repositories/User.repository';
-import { authenticate, encrypt } from '../utils/authManager';
+import { createUser, checkExistingUser, getUser } from '../repositories/User.repository';
+import { authenticate, encrypt, verify } from '../utils/authManager';
+import parser from '../mappers/loginBodyMapper';
 
 export const register = async (body) => {
 	try {
 		const { nickname, email } = body;
-		const exists = await checkExistingUser(nickname);
+		const exists = await checkExistingUser(nickname, email);
 		if (exists?.id) {
 			throw new AppError({
 				message: 'User already registered',
@@ -20,12 +21,30 @@ export const register = async (body) => {
 			password: await encrypt(body.password),
 		});
 		return {
-			user: {
-				nickname: registered.nickname,
-				email: registered.email,
-			},
+			user: { nickname: registered.nickname, email: registered.email },
 			token: authenticate(registered.id),
 		};
+	} catch (error) { throw new AppError(error); }
+};
+
+export const login = async (body) => {
+	try {
+		const credentials = parser(body);
+		const user = await getUser(credentials);
+		if (user) {
+			const validation = await verify(body.password, user.password);
+			if (validation) {
+				return {
+					user: { nickname: user.nickname, email: user.email },
+					token: authenticate(user.id),
+				};
+			}
+		}
+		throw new AppError({
+			message: 'Invalid credentials provided',
+			type: 'User-Invalid-Credentials',
+			status: 401,
+		});
 	} catch (error) {
 		throw new AppError(error);
 	}
