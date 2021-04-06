e2a8cccb59f3629530aed8d58f4b7124868d9d12
unimatrix2 <dnny.nis@gmail.com>
2021-04-06 16:45:18 -0300

    added repository functionality

---
+++ src/repositories/Post.repository.js
@@ -0,0 +1,49 @@
+/* eslint-disable comma-dangle */
+import Post from '../models/Post.model';
+import countMapper from '../mappers/postCountMapper';
+import AppError from '../errors/AppError';
+
+export const createPost = async (body) => {
+	try {
+		const newPost = new Post(body);
+		await newPost.save();
+		await Post.populate(newPost, { path: 'owner', select: 'nickname' });
+		return { owner: newPost.owner.nickname, content: newPost.content };
+	} catch (error) {
+		throw new AppError({
+			message: 'Could not create post',
+			type: 'Post-Repo-Create',
+			status: 400,
+		});
+	}
+};
+
+export const countPosts = async () => {
+	try {
+		const count = await Post.countDocuments();
+		return countMapper(count);
+	} catch (error) {
+		throw new AppError({
+			message: 'Could not count the posts',
+			type: 'Post-Count',
+			status: 400,
+		});
+	}
+};
+
+export const getPosts = async (start) => {
+	try {
+		const posts = await Post.find(
+			{},
+			['content', 'owner'],
+			{ sort: { createdAt: -1 }, skip: start, limit: 10 }
+		);
+		return posts;
+	} catch (error) {
+		throw new AppError({
+			message: 'Could not retrieve posts',
+			type: 'Posts-Get',
+			status: 500,
+		});
+	}
+};
