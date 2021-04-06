68797c566ba47e72306c3bcdf5c831e40e88fb6f
unimatrix2 <dnny.nis@gmail.com>
2021-04-06 19:37:32 -0300

    added post routes

---
+++ src/routes/post/post.routes.js
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { create, pages, retrieve } from '../../services/Post.service';
+
+const router = Router();
+
+router.post('/create', async (req, res) => {
+	const { body } = req;
+	const post = await create(body);
+	res.status(200).json(post);
+});
+
+router.get('/count', async (req, res) => {
+	try {
+		const count = await pages();
+		res.status(200).json({ pages: count });
+	} catch (error) {
+		res.status(error.status).json(error);
+	}
+});
+
+router.get('/board', async (req, res) => {
+	try {
+		const posts = await retrieve(null);
+		res.status(200).json(posts);
+	} catch (error) {
+		res.status(error.status).json(error);
+	}
+});
+
+router.get('/board/:page', async (req, res) => {
+	try {
+		const { page } = req.params;
+		const posts = await retrieve(Number(page));
+		res.status(200).json(posts);
+	} catch (error) {
+		res.status(error.status).json(error);
+	}
+});
+
+export default router;
