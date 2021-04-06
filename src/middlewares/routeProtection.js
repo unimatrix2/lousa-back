f3e3866df542b8300f3dde5f81b407e5bbe8c9c9
unimatrix2 <dnny.nis@gmail.com>
2021-04-06 15:56:05 -0300

    [FIX] protection now works more clearly

--- src/middlewares/routeProtection.js
+++ src/middlewares/routeProtection.js
@@ -1,27 +1,25 @@
+/* eslint-disable consistent-return */
 import AppError from '../errors/AppError';
 import { validate } from '../utils/authManager';
 
 const routeProtection = (req, res, nxt) => {
-	const { token } = req.signedCookies;
-	if (!token) {
-		req.error = new AppError({
-			message: 'No token provided',
-			type: 'Auth-No-Token',
-			status: 401,
-		});
-		return nxt();
-	}
 	try {
+		const { token } = req.signedCookies;
+		if (!token) {
+			throw new AppError({
+				message: 'No token provided',
+				type: 'Auth-No-Token',
+				status: 401,
+			});
+		}
 		const decoded = validate(token);
 		req.user = { id: decoded.id };
 		return nxt();
 	} catch (error) {
-		req.error = new AppError({
-			message: 'Expired or non-existent token',
-			type: 'Auth-Invalid-Token',
-			status: 401,
-		});
-		return nxt();
+		res
+			.clearCookie('token')
+			.status(error.status)
+			.json(new AppError(error));
 	}
 };
 
