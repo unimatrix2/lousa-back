cc85aa54d648fc318a674905b7d13af44b4d2e15
unimatrix2 <dnny.nis@gmail.com>
2021-04-06 16:44:57 -0300

    [LINTED] removed unused code

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,14 +1,12 @@
 import { Router } from 'express';
-import routeProtection from '../middlewares/routeProtection';
 import authRoutes from './auth/auth.routes';
-import userRoutes from './user/user.routes';
 import postRoutes from './post/post.routes';
+import routeProtection from '../middlewares/routeProtection';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use(routeProtection);
-router.use('/user', userRoutes);
 router.use('/post', postRoutes);
 
 export default router;
